6e65316ac4c6f63fd8be4339c06b4392e7f2fc5e
Andrea <andrear632@gmail.com>
2022-08-24 20:33:43 +0200

    .

--- docker/admin-server/admin-server.js
+++ docker/admin-server/admin-server.js
@@ -1,4 +1,17 @@
+const express = require('express');
 
-for(var i = 0; i <100; i++){
-    console.log(i)
-}
+//Deve ricevere post dall'admin e mandare websocket
+
+// Constants
+const PORT = process.env.PORT;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.post('/', (req, res) => {
+    console.log(`service: ${SERVICE_NAME} is called`);
+    res.send(`Hello world,Your in ${SERVICE_NAME}\n`);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
