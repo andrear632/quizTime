f53e63a7559610bbdf4ea5ac4a4f72386a560fea
Andrea <andrear632@gmail.com>
2022-08-25 17:04:58 +0200

    adminserverupdate

---
+++ admin_webapp/index.js
@@ -0,0 +1,39 @@
+async function send(){
+
+    var radios = document.getElementsByName("answer");
+    var correct = null;
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            correct = radios[i].value;
+            break;
+        }
+    }
+
+    if (correct==null) {
+        alert("Select a correct answer")
+    }
+    else {
+
+        var qn = document.getElementById("question");
+        var questionnumber = qn.value;
+
+        if (questionnumber=="") {
+            alert("Select question number")
+        }
+        else {
+            var data = {"correct":correct, "qn":questionnumber};
+            var url = "http://localhost:3000/start";
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data) // body data type must match "Content-Type" header
+            });
+            alert(response)
+        }
+
+    }
+
+}
--- docker/admin-server/admin-server.js
+++ docker/admin-server/admin-server.js
@@ -1,16 +1,51 @@
 const express = require('express');
+const app = express();
+
+var amqp = require('amqplib/callback_api');
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var cors = require('cors')
+app.use(cors())
 
 //Deve ricevere post dall'admin e mandare websocket
 
 // Constants
 const PORT = process.env.PORT;
 const HOST = '0.0.0.0';
+const URLRABBIT = "amqp://rabbitmq"
 
 // App
-const app = express();
-app.post('/', (req, res) => {
-    console.log(`service: ${SERVICE_NAME} is called`);
-    res.send(`Hello world,Your in ${SERVICE_NAME}\n`);
+
+app.post('/start', (req, res) => {
+    console.log('Question starting');
+    var info = req.body;
+
+    amqp.connect(URLRABBIT, function(error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function(error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+            var exchange = 'starts';
+            var msg = req.body;
+    
+            channel.assertExchange(exchange, 'fanout', {
+                durable: false
+            });
+            channel.publish(exchange, '', Buffer.from(msg));
+            console.log("Sent %s", msg);
+        });
+        setTimeout(function() {
+            connection.close();
+            process.exit(0);
+        }, 500);
+    });
+
+    res.send('Question starting');
 });
 
 app.listen(PORT, HOST);
