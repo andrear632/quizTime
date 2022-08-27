4fc6576cda83390ab9f382292429518363177bc2
alessandromarzilli99 <alessandromarzilli99@gmail.com>
2022-08-27 16:33:14 +0200

    .

--- docker/worker/worker.js
+++ docker/worker/worker.js
@@ -49,3 +49,28 @@ amqp.amqplisten();
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+
+
+// const { Client } = require('@elastic/elasticsearch')
+// const client = new Client({ node: 'http://elasticsearch:9200' })
+
+// async function run () {
+//   await client.index({
+//     index: 'game-of-thrones',
+//     id: '1',
+//     body: {
+//       character: 'Ned Stark',
+//       quote: 'Winter is coming.'
+//     }
+//   })
+
+//   const { body } = await client.get({
+//     index: 'game-of-thrones',
+//     id: '1'
+//   })
+
+//   console.log(body)
+// }
+
+// run().catch(console.log)
