dd73c75ad7972bbeeaeb8ed0c0f50505e4f3ea1e
andrea mazzitelli <mazzitelli.1835022@studenti.uniroma1.it>
2022-08-29 15:29:01 +0200

    first test dashboard import

--- docker/admin-server/initialize.sh
+++ docker/admin-server/initialize.sh
@@ -3,3 +3,4 @@
 
 curl -X DELETE 'elasticsearch:9200/game'
 curl -X PUT "elasticsearch:9200/game?pretty"
+curl -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_1.json" "http://kibana::5601/api/kibana/dashboards/import"
