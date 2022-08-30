23993019f97c724675c0e6ca31fc0812f2308ea9
andrea mazzitelli <mazzitelli.1835022@studenti.uniroma1.it>
2022-08-30 11:13:55 +0200

    initialize script changes

--- docker/admin-server/initialize.sh
+++ docker/admin-server/initialize.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-#Questo file è comodo se devo initializzare delle componenti del server
+#This file is used to initialize some elements at statup
+
+#wait for all the services to be online 
 sleep 20
+
+#purge old data
 curl -X DELETE 'elasticsearch:9200/game'
-curl -X PUT "elasticsearch:9200/game"
-curl -X PUT -H "Content-Type:application/json" -d '{"nickname":"admin","score":0}' 'elasticsearch:9200/game/_doc/0'
 curl -X DELETE 'elasticsearch:9200/questions'
+
+#create needed indexes
+curl -X PUT "elasticsearch:9200/game"
 curl -X PUT "elasticsearch:9200/questions"
+
+#create default entries
+curl -X PUT -H "Content-Type:application/json" -d '{"nickname":"admin","score":0}' 'elasticsearch:9200/game/_doc/0'
+curl -X PUT -H "Content-Type:application/json" -d '{}' 'elasticsearch:9200/questions/_doc/0'
+
+#import dashboards in kibana
 curl -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_1.json" "kibana:5601/api/kibana/dashboards/import"
+curl -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_2.json" "kibana:5601/api/kibana/dashboards/import"
--- docker/worker/initialize.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-#Questo file è comodo se devo initializzare delle componenti del server
