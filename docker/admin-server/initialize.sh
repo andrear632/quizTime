1849274d780c91dc99e989c4df65bcfccae7b4e9
Andrea <andrear632@gmail.com>
2022-08-30 12:00:48 +0200

    .

--- docker/admin-server/initialize.sh
+++ docker/admin-server/initialize.sh
@@ -4,28 +4,28 @@
 #wait for all the services to be online 
 sleep 20
 
-echo "End Sleep"
+echo "\nEnd Sleep\n"
 
 #purge old data
 curl -w -X DELETE 'elasticsearch:9200/game'
 curl -w -X DELETE 'elasticsearch:9200/questions'
 
-echo "Purged Indexes"
+echo "\nPurged Indexes\n"
 
 #create needed indexes
 curl -w -X PUT "elasticsearch:9200/game"
 curl -w -X PUT "elasticsearch:9200/questions"
 
-echo "Created Indexes"
+echo "\nCreated Indexes\n"
 
 #create default entries
 curl -w -X PUT -H "Content-Type:application/json" -d '{"nickname":"admin","score":0}' 'elasticsearch:9200/game/_doc/0'
-curl -w -X PUT -H "Content-Type:application/json" -d '{}' 'elasticsearch:9200/questions/_doc/0'
+curl -w -X PUT -H "Content-Type:application/json" -d '{"A":0, "B":0, "C":0, "D":0}' 'elasticsearch:9200/questions/_doc/0'
 
-echo "Created Default Entries"
+echo "\nCreated Default Entries\n"
 
 #import dashboards in kibana
 curl -w -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_1.json" "kibana:5601/api/kibana/dashboards/import"
 curl -w -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_2.json" "kibana:5601/api/kibana/dashboards/import"
 
-echo "Dashboard Imported"
+echo "\nDashboard Imported\n"
