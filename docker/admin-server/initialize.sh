0f2d64b25c46653e160c1bdd1f63d08fa6f8f23e
andrea mazzitelli <mazzitelli.1835022@studenti.uniroma1.it>
2022-08-30 11:59:32 +0200

    initialize changes

--- docker/admin-server/initialize.sh
+++ docker/admin-server/initialize.sh
@@ -4,18 +4,28 @@
 #wait for all the services to be online 
 sleep 20
 
+echo "End Sleep"
+
 #purge old data
-curl -X DELETE 'elasticsearch:9200/game'
-curl -X DELETE 'elasticsearch:9200/questions'
+curl -w -X DELETE 'elasticsearch:9200/game'
+curl -w -X DELETE 'elasticsearch:9200/questions'
+
+echo "Purged Indexes"
 
 #create needed indexes
-curl -X PUT "elasticsearch:9200/game"
-curl -X PUT "elasticsearch:9200/questions"
+curl -w -X PUT "elasticsearch:9200/game"
+curl -w -X PUT "elasticsearch:9200/questions"
+
+echo "Created Indexes"
 
 #create default entries
-curl -X PUT -H "Content-Type:application/json" -d '{"nickname":"admin","score":0}' 'elasticsearch:9200/game/_doc/0'
-#curl -X PUT -H "Content-Type:application/json" -d '{}' 'elasticsearch:9200/questions/_doc/0'
+curl -w -X PUT -H "Content-Type:application/json" -d '{"nickname":"admin","score":0}' 'elasticsearch:9200/game/_doc/0'
+curl -w -X PUT -H "Content-Type:application/json" -d '{}' 'elasticsearch:9200/questions/_doc/0'
+
+echo "Created Default Entries"
 
 #import dashboards in kibana
-curl -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_1.json" "kibana:5601/api/kibana/dashboards/import"
-#curl -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_2.json" "kibana:5601/api/kibana/dashboards/import"
+curl -w -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_1.json" "kibana:5601/api/kibana/dashboards/import"
+curl -w -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_2.json" "kibana:5601/api/kibana/dashboards/import"
+
+echo "Dashboard Imported"
