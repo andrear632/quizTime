f2ed338ea2798521e3ed0691ce0360f964ef477b
andrea mazzitelli <mazzitelli.1835022@studenti.uniroma1.it>
2022-08-30 11:31:37 +0200

    maybe fix

--- docker/admin-server/initialize.sh
+++ docker/admin-server/initialize.sh
@@ -14,7 +14,7 @@ curl -X PUT "elasticsearch:9200/questions"
 
 #create default entries
 curl -X PUT -H "Content-Type:application/json" -d '{"nickname":"admin","score":0}' 'elasticsearch:9200/game/_doc/0'
-curl -X PUT -H "Content-Type:application/json" -d '{}' 'elasticsearch:9200/questions/_doc/0'
+#curl -X PUT -H "Content-Type:application/json" -d '{}' 'elasticsearch:9200/questions/_doc/0'
 
 #import dashboards in kibana
 curl -X POST -H "kbn-xsrf: reporting" -H "Content-Type:application/json" -d "@kibana_dashboard_1.json" "kibana:5601/api/kibana/dashboards/import"
