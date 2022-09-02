45d6b95afbb442c809469db48ad309ed7a7c199a
Andrea <andrear632@gmail.com>
2022-09-02 16:08:30 +0200

    .

--- docker/worker/db_functions.js
+++ docker/worker/db_functions.js
@@ -30,7 +30,8 @@ async function update(id, score){
                 newScore: score
             }
           }
-        }
+        },
+        retry_on_conflict: 10
     })
 }
 
@@ -43,7 +44,8 @@ async function update_questions(id, letter){
           lang: "painless",
           source: "ctx._source."+letter+" ++"
         }
-      }
+      },
+      retry_on_conflict: 10
   })
 }
 
