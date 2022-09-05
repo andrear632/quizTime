16eee7dd47a71cf3ba7df9ab353b15b066242f5f
Andrea <andrear632@gmail.com>
2022-09-05 10:51:36 +0200

    .

--- docker/webserver-admin/admin_webapp/index.js
+++ docker/webserver-admin/admin_webapp/index.js
@@ -28,7 +28,7 @@ async function send(){
         }
         else {
             var data = {'correct':correct, 'qn':questionnumber};
-            var url = "https://loadBalancer2-e59f5f735dc34e59.elb.us-east-1.amazonaws.com:443/start";
+            var url = "https://quizload-af9d17368b7ae969.elb.us-east-1.amazonaws.com:443/start";
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
@@ -52,7 +52,7 @@ async function send(){
 async function end(){
 
             var data = {'game':'end'};
-            var url = "https://loadBalancer2-e59f5f735dc34e59.elb.us-east-1.amazonaws.com:443/end";
+            var url = "https://quizload-af9d17368b7ae969.elb.us-east-1.amazonaws.com:443/end";
             const response = await fetch(url, {
                 method: 'POST',
                 headers: {
